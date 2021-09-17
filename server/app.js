887104ac6fc1ac870c62ee00e8040bddb24da645
Zane Finucane-Shook <zane@Zanes-Mac-mini.local>
2021-09-16 17:41:46 -0700

    slow day backend started setting up server to test connection to mongodb

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+const express = require('express')
+
+const mongoose = require('./config/database')
+
+const app= express()
+const port =3000
+1
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+app.listen(port, ()=>{
+console.log(`app listening at http://lochost:${port}`)
+
+}) 
+
