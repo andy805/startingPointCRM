95d5ef32ab938cc7960d30967fc100b5997e7f29
Zane Finucane-Shook <zane@Zanes-Mac-mini.local>
2021-09-16 17:41:46 -0700

    slow day backend started setting up server to test connection to mongodb

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.Database_Url,{ useNewUrlParser: true})
+mongoose.connect(process.env.Database_Url,{ useNewUrlParser: true, useUnifedToplogy = true})
 
-mongoose.connection.on('connected', fun)
+const db= mongoose.connection
+
+mongoose.connection.on('connected', function(){
+console.log(`Mongoose connect to: ${process.env.Database_URL}`)
+})
+ 
+module.exports = mongoose
