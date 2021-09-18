de4107519ab37cc2253e0e7f6b84566d9bee98c7
Andy <andyvelazquez13@yahoo.com>
2021-09-17 22:07:30 -0700

    add grid to dashboard, add subgrid to buttons , add fontawesome, add css modules

---
+++ client/src/components/UI/Card.js
@@ -0,0 +1,16 @@
+
+
+import styles from './Card.module.css';
+
+const Card = (props) => {
+
+    const classes = styles.card +" "+ props.className
+
+    return (
+        <div className={classes}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import DashboardButton from '../components/DashboardButton';
+import styles from './Dashboard.module.css';
+import Card from '../components/UI/Card'
 import { useHistory } from 'react-router-dom';
 
 
@@ -14,14 +17,15 @@ const Dashboard = (props) => {
     }
 
     return (
-        <div>
+        <div className={styles.dashboard}>
 
-            <button onClick={goToAccounts}>Account</button>
-            <button onClick={goToContacts}>Contact</button>
-            <button>Invoice</button>
-            <button>Products</button>
-            <button>Preferences</button>
-
+            <Card className={styles["card-button-area"]}> 
+                <DashboardButton className={styles["card-grid-cell-center"] +" "+ styles["card-grid-cell-one-one"]} history={props.historyLayout} path={"/Accounts"} label={"Account"}/>
+                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-two"]}`} history={props.historyLayout} path={"/Contacts"} label={"Contacts"}/>
+                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-three"]}`} history={props.historyLayout} path={"/Invoices"} label={"Invoices"}/>
+                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-one"]}`} history={props.historyLayout} path={"/Products"} label={"Products"}/>
+                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-two"]}`} history={props.historyLayout} path={"/System"} label={"System"}/>
+            </Card>
         </div>
 
     )
