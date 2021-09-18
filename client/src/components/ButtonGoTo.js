91bc18c485305bbded12dd0c676de14b7dc91d7c
Andy <andyvelazquez13@yahoo.com>
2021-09-17 18:25:15 -0700

    add contact and account pages, add the routing to those pages, add useHistory

---
+++ client/src/components/ButtonGoTo.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const ButtonGoTo = () => {
+
+    return (
+        <button>Button Go To</button>
+    )
+}
+
+export default ButtonGoTo;
---
+++ client/src/pages/Accounts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useHistory } from "react-router";
+
+const Accounts = () => {
+
+    return(
+        <h1>Accounts</h1>
+    )
+
+}
+
+export default Accounts;
