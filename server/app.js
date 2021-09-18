6d8e8708a346471c5ddcefad7e17d3fc6a022d74
Zane Finucane-Shook <zane@Zanes-Mac-mini.local>
2021-09-17 18:19:44 -0700

    git ignore update

--- server/app.js
+++ server/app.js
@@ -2,16 +2,16 @@ const express = require('express')
 require('dotenv').config('.env')
 
 const mongoose = require('./config/database')
-const app= express()
-const port =3000
+const app = express()
+const port = 3000
 1
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(port, ()=>{
-console.log(`app listening at http://lochost:${port}`)
+app.listen(port, () => {
+    console.log(`app listening at http://lochost:${port}`)
 
-}) 
+})
 
