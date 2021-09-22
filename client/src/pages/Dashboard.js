7ba0dbf0aa90d84c14550998584455827691493a
Andy <andyvelazquez13@yahoo.com>
2021-09-21 19:56:19 -0700

    change dashboardButton component to ButtonWithIcon component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,6 +3,7 @@ import DashboardButton from '../components/DashboardButton';
 import styles from './Dashboard.module.css';
 import Card from '../components/UI/Card'
 import { useHistory } from 'react-router-dom';
+import ButtonWithIcon from '../components/ButtonWithIcon.js';
 
 
 const Dashboard = (props) => {
@@ -20,11 +21,13 @@ const Dashboard = (props) => {
         <div className={styles.dashboard}>
 
             <Card className={styles["card-button-area"]}> 
-                <DashboardButton className={styles["card-grid-cell-center"] +" "+ styles["card-grid-cell-one-one"]} history={props.historyLayout} path={"/Accounts"} label={"Account"}/>
-                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-two"]}`} history={props.historyLayout} path={"/Contacts"} label={"Contacts"}/>
-                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-three"]}`} history={props.historyLayout} path={"/Invoices"} label={"Invoices"}/>
-                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-one"]}`} history={props.historyLayout} path={"/Products"} label={"Products"}/>
-                <DashboardButton className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-two"]}`} history={props.historyLayout} path={"/System"} label={"System"}/>
+                <ButtonWithIcon className={styles["card-grid-cell-center"] +" "+ styles["card-grid-cell-one-one"]} icon={"users"} history={props.historyLayout} path={"/Accounts"} label={"Account"}/>
+                <ButtonWithIcon className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-two"]}`} icon={"user"}  history={props.historyLayout} path={"/Contacts"} label={"Contacts"}/>
+                <ButtonWithIcon className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-one-three"]}`} icon={"file-invoice-dollar"} history={props.historyLayout} path={"/Invoices"} label={"Invoices"}/>
+                <ButtonWithIcon className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-one"]}`} icon={"boxes"} history={props.historyLayout} path={"/Products"} label={"Products"}/>
+                <ButtonWithIcon className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-two-two"]}`} icon={"cog"} history={props.historyLayout} path={"/System"} label={"System"}/>
+                <ButtonWithIcon className={`${styles["card-grid-cell-center"]} ${styles["card-grid-cell-three-three"]}`} icon={"users"} label={"Accounts"} history={props.historyLayout} path={"/Account"}></ButtonWithIcon>
+                
             </Card>
         </div>
 
