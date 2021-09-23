212cb8d3f74a03455e195b5144a15aa9e8930c05
Gary <gvk795@gmail.com>
2021-09-23 16:47:53 -0700

    bg color for account status made dynamic

--- client/src/components/UI/PortalRow.js
+++ client/src/components/UI/PortalRow.js
@@ -1,24 +1,29 @@
-import react, { useState } from 'react'
-import Card from './Card'
-import styles from './PortalRow.module.css'
-
+import react, { useState } from "react";
+import Card from "./Card";
+import styles from "./PortalRow.module.css";
 
 const PortalRow = () => {
-    const [masterRecords, setmasterRecords] = useState(['adam', 'mark', 'joe',])
-    
-        return(
-            <div className= {styles.portalRow}>
-                <div className={`${styles.status} ${styles.statusActive}`}>
-                <div className={styles.statusText}>Active</div>
 
-                </div>
-                <div className = {styles.headerLabel}>this should be small text above</div>
-                <div className = {styles.mainLabel}>Main text</div>
-                <div className = {styles.subLabel}>some text underneathe</div>
+    // TODO: Status should probably come from redux or some shit not useState
+    const [status, setStatus] = useState(["Inactive"]);
+
+    return (
+        <div className={styles.portalRow}>
+
+            {/* status attribute changes css classes for background color */}
+            <div className={`${styles.status} ${styles.statusBackground}`} status={status}>
+                <div className={styles.statusText}>{status}</div>
             </div>
-            
-        )
-    
-    }
-    
-    export default PortalRow;
+
+            <div className={styles.headerLabel}>
+                this should be small text above
+            </div>
+
+            <div className={styles.mainLabel}>Main text</div>
+            <div className={styles.subLabel}>some text underneath</div>
+
+        </div>
+    );
+};
+
+export default PortalRow;
