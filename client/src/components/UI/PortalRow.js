c5b8d8f81cf314f08f9d529a319a67792c28b15d
laststandjam <zaneshook@gmail.com>
2021-09-23 15:01:28 -0700

    text aligment

--- client/src/components/UI/PortalRow.js
+++ client/src/components/UI/PortalRow.js
@@ -1,17 +1,20 @@
 import react, { useState } from 'react'
 import Card from './Card'
-import style from './PortalRow.module.css'
+import styles from './PortalRow.module.css'
 
 
 const PortalRow = () => {
-    const [masterRecords, setmasterRecords] = useState(['adam', 'mark', 'joe'])
+    const [masterRecords, setmasterRecords] = useState(['adam', 'mark', 'joe',])
     
         return(
-            <div className= {style.portalRow}>
-                <div className={style.status}>Active</div>
-                <div className = {style.headerLabel}>this should be small text above</div>
-                <div className = {style.mainLabel}>Main text</div>
-                <div className = {style.subLabel}>some text underneathe</div>
+            <div className= {styles.portalRow}>
+                <div className={`${styles.status} ${styles.statusActive}`}>
+                <div className={styles.statusText}>Active</div>
+
+                </div>
+                <div className = {styles.headerLabel}>this should be small text above</div>
+                <div className = {styles.mainLabel}>Main text</div>
+                <div className = {styles.subLabel}>some text underneathe</div>
             </div>
             
         )
