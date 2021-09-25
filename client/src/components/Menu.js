ebc2da17e2c0d8a41c8ea354ef4cf6bbcbfc7f8d
Gary <gvk795@gmail.com>
2021-09-25 10:06:31 -0700

    styling and line breaks added to menu component

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import styles from './Menu.module.css';
+import React from "react";
+import styles from "./Menu.module.css";
 
 export default function Menu() {
     return (
-        <div className={`${styles.menu} ${'boxshadow'} ${'flex-center'}`}>
+        <div className={`${styles.menu} ${"boxshadow"} ${"flex-center"}`}>
             <label>Name</label>
             <input type="text" />
+            <div className={styles.menu__lineBreak} />
 
             <label>Type</label>
             <select name="" id="" placeholder="Choose..."></select>
+            <div className={styles.menu__lineBreak} />
 
             <div className={styles.menu__buttons}>
                 <button>Active</button>
                 <button className={styles.menu__middleButton}>Inactive</button>
                 <button>Disregard</button>
             </div>
-
         </div>
-    )
+    );
 }
