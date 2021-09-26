169663deaae7caed8b5e95eca9e4905e86a15b1e
laststandjam <zaneshook@gmail.com>
2021-09-25 20:58:11 -0700

    starting input forms in menu

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,9 +1,11 @@
 import express from "express";
-import index from "../controllers/accounts.js"
+import {index, createAccount} from "../controllers/accounts.js"
 
 const router = express.Router()
 
 
 router.get('/', index)
+router.post('/create', createAccount )
+
 
 export {router as accountRouter}
