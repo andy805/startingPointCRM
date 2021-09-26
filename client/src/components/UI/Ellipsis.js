cb2afe77e33ef5baf450d9b0e6b66623388970da
Gary <gvk795@gmail.com>
2021-09-25 21:35:23 -0700

    css file created for Ellipsis component

--- client/src/components/UI/Ellipsis.js
+++ client/src/components/UI/Ellipsis.js
@@ -1,22 +1,10 @@
 import React from "react";
+import styles from "./Ellipsis.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
-// This variable lets you dynamically change size of circle
-let width = "6px";
-
-const ellipsis = {
-    fontSize: "13px",
-    backgroundColor: "#f2f2f2",
-    color: "var(--fmsp-blue)",
-    padding: `${width}`,
-    borderRadius: `calc(9px + ${width})`,
-};
-
 function Ellipsis() {
-    return (
-        <FontAwesomeIcon icon={faEllipsisH} style={ellipsis}></FontAwesomeIcon>
-    );
+    return <FontAwesomeIcon icon={faEllipsisH}></FontAwesomeIcon>;
 }
 
 export default Ellipsis;
