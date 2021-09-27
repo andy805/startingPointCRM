c033b48ecac33ca54dcff0bfc0f6081565da06ed
Andy <andyvelazquez13@yahoo.com>
2021-09-26 22:28:04 -0700

    add click feature to master detail

--- client/src/components/UI/Masterdetail.js
+++ client/src/components/UI/Masterdetail.js
@@ -3,16 +3,35 @@ import Card from "./Card";
 import style from "./Masterdetail.module.css";
 import PortalRow from "./PortalRow";
 
-const Masterdetail = ({ masterRecords }) => {
+const Masterdetail = ({ masterRecords , cardClick}) => {
+
+    // const [activeRow , setActiveRow] = useState({data: masterRecords, activeCard: 0});
+    // console.log(activeRow)
+
+    // const setActivePortalRow = (rowNumber) => {
+    //     console.log(rowNumber);
+    //     // setActiveRow(Number(rowNumber));
+
+    // }
+
+    const cardClickMaster = id => {
+        cardClick(id);
+    }
+
     return (
         <div className={style.masterDetail}>
-            {masterRecords.map((masterRecord) => (
+            {masterRecords.data.map((masterRecord, index) => (
                 <PortalRow
                     className={style.PortalRow}
                     status={masterRecord.status}
                     headerLabel={masterRecord.accountInfo}
                     mainLabel={masterRecord.accountName}
                     phone1={masterRecord.phone1}
+                    state={index===masterRecords.active? "active" : "inactive"}
+                    rowNumber={Number(index)}
+                    // key={index}
+                    // setPortalRowState={setActivePortalRow}
+                    cardClick={cardClickMaster}
                 />
             ))}
         </div>
