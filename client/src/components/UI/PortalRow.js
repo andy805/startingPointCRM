0fbfaf0dcd299048e90f5fc089ed3caf8cddce70
Andy <andyvelazquez13@yahoo.com>
2021-09-26 22:28:04 -0700

    add click feature to master detail

--- client/src/components/UI/PortalRow.js
+++ client/src/components/UI/PortalRow.js
@@ -2,9 +2,27 @@ import react, { useState } from "react";
 import Card from "./Card";
 import styles from "./PortalRow.module.css";
 
-const PortalRow = ({ status, headerLabel, mainLabel, phone1 }) => {
+const PortalRow = ({status, headerLabel, mainLabel, phone1, state, rowNumber, setPortalRowState, cardClick}) => {
+
+    const [statusAccount, setStatusAccount] = useState(status);
+    const [activeRow , setActiveRow] = useState(state);
+
+    // console.log("index", index);
+    // console.log("props", props);
+
+    // const setActivePortalRow = () => {
+    //     setActiveRow("active");
+    //     props.setPortalRowState(Number(props.rowNumber));
+    //     console.log("test",Number(props.rowNumber));
+
+    // }
+
+    const rowOnClickHandler = (ev) => {
+        cardClick(rowNumber);
+    }
+
     return (
-        <div className={`${styles.portalRow} ${"box-shadow"}`}>
+        <div className={state === "active" ? styles.portalRow : styles.portalRowInactive} onClick={rowOnClickHandler} >
             <div
                 className={`${styles.status} ${styles.statusBackground}`}
                 status={status} // changes css classes for left edge of portal row
@@ -13,7 +31,9 @@ const PortalRow = ({ status, headerLabel, mainLabel, phone1 }) => {
             </div>
 
             <div className={styles.labelContainer}>
-                <div className={styles.headerLabel}>{headerLabel}</div>
+                <div className={styles.headerLabel}>
+                    {headerLabel}
+                </div>
 
                 <div className={styles.mainLabel}>{mainLabel}</div>
                 <div className={styles.subLabel}>{phone1}</div>
--- client/src/pages/Accounts.js
+++ client/src/pages/Accounts.js
@@ -1,68 +1,81 @@
-import React, { useEffect, useState } from "react";
-import Masterdetail from "../components/UI/Masterdetail";
-import styles from "./pages.module.css";
-import TopNav from "../components/UI/TopNav.js";
-import Navbar from "../components/Navbar";
-import axios from "axios";
-import Menu from "../components/Menu";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router";
+import Masterdetail from "../components/UI/Masterdetail"
+import styles from "./pages.module.css"
+import TopNav from '../components/UI/TopNav.js';
+import Navbar from '../components/Navbar';
+import axios from 'axios'
 
-const navItems = [
-    { label: "Accounts", path: "/Accounts", active: true },
-    { label: "Contacts", path: "/Contacts", active: false },
-    { label: "Invoices", path: "/Invoices", active: false },
-    { label: "Products", path: "/Products", active: false },
-];
+const navItems = [ {label: "Accounts" , path: "/Accounts", active: true}, 
+{label: "Contacts" , path: "/Contacts", active: false} ,{label: "Invoices" , path: "/Invoices", active: false}, 
+{label: "Products" , path: "/Products", active: false} 
+]
 
 // fetches accounts
-const accountArray = [];
-const firstAccount = [];
-const Accounts = () => {
-    const [accounts, setAccounts] = useState(accountArray);
-    
-    //place holder untill we chaing ids 
-    // const [currentAccount,setCurrentAccount ] = useState(firstAccount)
-    
-    
+const accountArray = [ 
+        // {
+        //     status: "Active",
+        //     accountInfo: "ACT001 > Company > Santa Clara (CA)",
+        //     accountName: "Carl's Jr. Consulting",
+        //     phone1: "(243) 932-5834"
+        // },
+        // {
+        //     status: "Inactive",
+        //     accountInfo: "ACT002 > Company > Los Angeles (CA)",
+        //     accountName: "Molestie Sed Foundation",
+        //     phone1: "(666) 420-6969"
+        // },
+        // {
+        //     status: "Disregard",
+        //     accountInfo: "ACT003 > Company > Las Vegas (NV)",
+        //     accountName: "Arcu Sed Institute",
+        //     phone1: "(437) 890-4563"
+        // }
+    ]
 
-    useEffect(() => {
-        const fetchAccounts = async () => {
-            const response = await axios.get("http://localhost:3000/accounts");
-            
-            setAccounts([...response.data]);
-        };
-        fetchAccounts();
-    }, []);
+
+const Accounts = () => {
+    
+    const [masterRecords, setMasterRecords] = useState({data: accountArray , active: 0});
+    
+    const addAccounts = (accounts) => {                               
+                 setMasterRecords(prevState => ({
+                      ...prevState, data: [...prevState.data , ...accounts]
+                 }));
+             }
+    const changeActiveStateHandler = (i) => {
+        setMasterRecords(prevState => ({
+            ...prevState , active: i
+        }));
+    }
+    
+    useEffect (() =>{
+
+
+      const fetchAccounts = async ()=>{
+          
+          const response = await axios.get("http://localhost:3000")
+        console.log('hit', response.data)
+        addAccounts(response.data)
+        
+
+      }
+      fetchAccounts()
+    },[])
 
     // Dummy JSON until database is ready
-    // const [masterRecords] = useState([
-    //     {
-    //         status: "Active",
-    //         accountInfo: "ACT001 > Company > Santa Clara (CA)",
-    //         accountName: "Carl's Jr. Consulting",
-    //         phone1: "(243) 932-5834",
-    //     },
-    //     {
-    //         status: "Inactive",
-    //         accountInfo: "ACT002 > Company > Los Angeles (CA)",
-    //         accountName: "Molestie Sed Foundation",
-    //         phone1: "(666) 420-6969",
-    //     },
-    //     {
-    //         status: "Disregard",
-    //         accountInfo: "ACT003 > Company > Las Vegas (NV)",
-    //         accountName: "Arcu Sed Institute",
-    //         phone1: "(437) 890-4563",
-    //     },
-    // ]);
-    return (
+    console.log(masterRecords)
+
+    return(
         <div className={styles.page}>
             <TopNav className={styles.navbar}>
                 <Navbar navItems={navItems}></Navbar>
             </TopNav>
-            <Menu account={accounts}/>
-            <Masterdetail masterRecords={accounts} />
+            <h1>Accounts</h1>
+            <Masterdetail masterRecords={masterRecords} cardClick={changeActiveStateHandler}/>
         </div>
-    );
-};
+    )
+
+}
 
 export default Accounts;
