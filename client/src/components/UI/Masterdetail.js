eb4eb6d53a8f308e9fd74e53b6a1d892f2e67122
Gary <gvk795@gmail.com>
2021-09-29 01:06:34 -0700

    Ellipsis and plus icon added to masterdetail

--- client/src/components/UI/Masterdetail.js
+++ client/src/components/UI/Masterdetail.js
@@ -2,17 +2,12 @@ import react, { useState } from "react";
 import Card from "./Card";
 import style from "./Masterdetail.module.css";
 import PortalRow from "./PortalRow";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 
 const Masterdetail = ({ masterRecords , cardClick}) => {
-
-    // const [activeRow , setActiveRow] = useState({data: masterRecords, activeCard: 0});
-    // console.log(activeRow)
-
-    // const setActivePortalRow = (rowNumber) => {
-    //     console.log(rowNumber);
-    //     // setActiveRow(Number(rowNumber));
-
-    // }
 
     const cardClickMaster = id => {
         cardClick(id);
@@ -20,6 +15,13 @@ const Masterdetail = ({ masterRecords , cardClick}) => {
 
     return (
         <div className={style.masterDetail}>
+            
+            {/* This class name could be better, let me know if you have suggestions */}
+            <div className={style.masterDetail__buttons}>
+              <FontAwesomeIcon icon={faEllipsisH}></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+            </div>
+
             {masterRecords.data.map((masterRecord, index) => (
                 <PortalRow
                     className={style.PortalRow}
@@ -29,8 +31,6 @@ const Masterdetail = ({ masterRecords , cardClick}) => {
                     phone1={masterRecord.phone1}
                     state={index===masterRecords.active? "active" : "inactive"}
                     rowNumber={Number(index)}
-                    // key={index}
-                    // setPortalRowState={setActivePortalRow}
                     cardClick={cardClickMaster}
                 />
             ))}
