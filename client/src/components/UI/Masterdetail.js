565501d23f10c7c294322337befcc4495b5f5105
Gary <gvk795@gmail.com>
2021-09-29 20:15:33 -0700

    bottom menu added to master detail component

--- client/src/components/UI/Masterdetail.js
+++ client/src/components/UI/Masterdetail.js
@@ -5,9 +5,18 @@ import PortalRow from "./PortalRow";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 
 const Masterdetail = ({ masterRecords , cardClick}) => {
+
+    // Dummy variables for bottom menu
+    const bottomMenuData = {
+        activeRecordNumber: 1,
+        foundCount: 33,
+        totalRecordCount: 106,
+        sortState: "Unsorted",
+    }
 
     const cardClickMaster = id => {
         cardClick(id);
@@ -16,10 +25,13 @@ const Masterdetail = ({ masterRecords , cardClick}) => {
     return (
         <div className={style.masterDetail}>
             
-            {/* This class name could be better, let me know if you have suggestions */}
-            <div className={style.masterDetail__buttons}>
-              <FontAwesomeIcon icon={faEllipsisH}></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+            <div className={style.masterDetail__bottomMenu}>
+                <FontAwesomeIcon icon={faCircleNotch}></FontAwesomeIcon>
+                <div className={style.masterDetail__record}>
+                  {bottomMenuData.activeRecordNumber} / {bottomMenuData.foundCount} Found ({bottomMenuData.totalRecordCount} Total) {bottomMenuData.sortState}
+                </div>
+                <FontAwesomeIcon icon={faEllipsisH}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
             </div>
 
             {masterRecords.data.map((masterRecord, index) => (
