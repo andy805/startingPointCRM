a17fd8c93177a61f558a1465c34cd368ce1053a8
laststandjam <zaneshook@gmail.com>
2021-09-30 16:35:58 -0700

    updating account

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {index, createAccount} from "../controllers/accounts.js"
+import {index, createAccount, updateAccount} from "../controllers/accounts.js"
 
 const router = express.Router()
 
 
 router.get('/', index)
 router.post('/create', createAccount )
+router.put('/update', updateAccount )
 
 
 export {router as accountRouter}
