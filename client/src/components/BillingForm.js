55a897c1358deaa2c5d491766aeba15dc9a1fdfa
Gary <gvk795@gmail.com>
2021-09-30 00:33:20 -0700

    styling added to accordion

--- client/src/components/BillingForm.js
+++ client/src/components/BillingForm.js
@@ -4,12 +4,12 @@ import EditBox from './UI/EditBox';
 const BillingForm = (props) => {
     return (
         <>
-            <EditBox fieldName={"Address 1"} fieldData={props.activeRecord.billingAddress1}></EditBox>
-            <EditBox fieldName={"Address 2"} fieldData={props.activeRecord.billingAddress2}></EditBox>
-            <EditBox fieldName={"City"} fieldData={props.activeRecord.billingCity}></EditBox>
-            <EditBox fieldName={"State"} fieldData={props.activeRecord.billingState}></EditBox>
-            <EditBox fieldName={"Postal Code"} fieldData={props.activeRecord.billingZipcode}></EditBox>
-            <EditBox fieldName={"Country"} fieldData={props.activeRecord.billingCountry}></EditBox>
+            <EditBox fieldName={"Address 1"} fieldData={props.activeRecord.billingAddress1} />
+            <EditBox fieldName={"Address 2"} fieldData={props.activeRecord.billingAddress2} />
+            <EditBox fieldName={"City"} fieldData={props.activeRecord.billingCity} />
+            <EditBox fieldName={"State"} fieldData={props.activeRecord.billingState} />
+            <EditBox fieldName={"Postal Code"} fieldData={props.activeRecord.billingZipcode} />
+            <EditBox fieldName={"Country"} fieldData={props.activeRecord.billingCountry} lineBreak={false}/>
         </>
     )
 }
