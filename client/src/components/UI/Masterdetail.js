c629f502c4f5f9677e126198cc6a8e0d2cc3f948
Gary <gvk795@gmail.com>
2021-09-29 21:26:55 -0700

    push button active styling

--- client/src/components/UI/Masterdetail.js
+++ client/src/components/UI/Masterdetail.js
@@ -6,29 +6,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import PushButton from "./PushButton";
 
-
-const Masterdetail = ({ masterRecords , cardClick}) => {
-
+const Masterdetail = ({ masterRecords, cardClick }) => {
     // Dummy variables for bottom menu
     const bottomMenuData = {
         activeRecordNumber: 1,
         foundCount: 33,
         totalRecordCount: 106,
         sortState: "Unsorted",
-    }
+    };
 
-    const cardClickMaster = id => {
+    const cardClickMaster = (id) => {
         cardClick(id);
-    }
+    };
 
     return (
         <div className={style.masterDetail}>
-            
+            <div className={style.masterDetail__topMenu}>
+                <PushButton />
+            </div>
+
             <div className={style.masterDetail__bottomMenu}>
                 <FontAwesomeIcon icon={faCircleNotch}></FontAwesomeIcon>
                 <div className={style.masterDetail__record}>
-                  {bottomMenuData.activeRecordNumber} / {bottomMenuData.foundCount} Found ({bottomMenuData.totalRecordCount} Total) {bottomMenuData.sortState}
+                    {bottomMenuData.activeRecordNumber} /{" "}
+                    {bottomMenuData.foundCount} Found (
+                    {bottomMenuData.totalRecordCount} Total){" "}
+                    {bottomMenuData.sortState}
                 </div>
                 <FontAwesomeIcon icon={faEllipsisH}></FontAwesomeIcon>
                 <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
@@ -41,7 +46,9 @@ const Masterdetail = ({ masterRecords , cardClick}) => {
                     headerLabel={masterRecord.accountInfo}
                     mainLabel={masterRecord.accountName}
                     phone1={masterRecord.phone1}
-                    state={index===masterRecords.active? "active" : "inactive"}
+                    state={
+                        index === masterRecords.active ? "active" : "inactive"
+                    }
                     rowNumber={Number(index)}
                     cardClick={cardClickMaster}
                 />
