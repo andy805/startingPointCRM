8be5c289d4d6d76824a080c2f62964e46eb818b4
laststandjam <zaneshook@gmail.com>
2021-09-30 20:30:26 -0700

    updating billing adress working

--- client/src/components/UI/EditBox.js
+++ client/src/components/UI/EditBox.js
@@ -1,24 +1,30 @@
 import React from "react";
 import styles from "./EditBox.module.css";
 
-function EditBox({
+
+const EditBox = ({
     fieldName,
     fieldData,
     activeRecord,
+    handleChange,
+    keyName,
     width = { width: "100%" },
     lineBreak = true,
-}) {
+})=>{
     return (
         <div className={styles.editBox}>
             <label className={styles.label}>{fieldName}</label>
             <input
+            onChange={handleChange}
                 type="text"
-                value={activeRecord?.accountName}
+                name={keyName}
                 style={width}
             />
             <div className={lineBreak ? styles.editBox__lineBreak : ""} />
         </div>
     );
+
 }
+
 
 export default EditBox;
