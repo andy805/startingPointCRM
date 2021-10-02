c9d31bca90a53c778d2ac56772b9c8af060743e5
Gary <gvk795@gmail.com>
2021-10-02 13:26:38 -0700

    icons added for top nav component

--- client/src/components/UI/TopNav.js
+++ client/src/components/UI/TopNav.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styles from './TopNav.module.css';
+import HomeSVG from "../SVG/HomeSVG";
+import CalendarSVG from "../SVG/CalendarSVG";
+import ToDoSVG from "../SVG/ToDoSVG";
 
 const TopNav = (props) => {
-
-   const classes =  styles.topnav + " " + props.className
+    const classes = styles.topnav + " " + props.className;
 
     return (
         <div className={classes}>
+            {/* <HomeSVG /> */}
+            {/* <CalendarSVG /> */}
+            {/* <ToDoSVG /> */}
             {props.children}
         </div>
-    )
-}
+    );
+};
 
 export default TopNav;
