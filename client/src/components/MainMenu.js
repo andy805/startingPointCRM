ca203e79ed07ea462f651054bcc02959f149457e
Gary <gvk795@gmail.com>
2021-10-06 01:08:17 -0700

    labels added to pushbutton components in mainmenu

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -16,13 +16,13 @@ const MainMenu = () => {
         <div className={`${styles.mainMenu} ${'box-shadow'}`}>
             <div className={styles.mainMenu__buttons}>
                 <PushButton svgElement={<ContactsSVG />} width={"73px"} height={"61px"} title={"Contacts"} counterField={0} />
-                <PushButton svgElement={<EstimatesSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<InvoicesSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<ProjectsSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<TasksSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<ProductsSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<NotesSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<FilesSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<EstimatesSVG />} width={"73px"} height={"61px"} title={"Estimates"} counterField={0} />
+                <PushButton svgElement={<InvoicesSVG />} width={"73px"} height={"61px"} title={"Invoices"} counterField={0} />
+                <PushButton svgElement={<ProjectsSVG />} width={"73px"} height={"61px"} title={"Projects"} counterField={0} />
+                <PushButton svgElement={<TasksSVG />} width={"73px"} height={"61px"} title={"Tasks"} counterField={0} />
+                <PushButton svgElement={<ProductsSVG />} width={"73px"} height={"61px"} title={"Products"} counterField={0} />
+                <PushButton svgElement={<NotesSVG />} width={"73px"} height={"61px"} title={"Notes"} counterField={0} />
+                <PushButton svgElement={<FilesSVG />} width={"73px"} height={"61px"} title={"Files"} counterField={0} />
             </div>
         </div>        
     )
