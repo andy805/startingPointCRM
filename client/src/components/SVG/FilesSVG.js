a6be038837934cf94886ab9630bfc4400037c502
Gary <gvk795@gmail.com>
2021-10-05 23:32:10 -0700

    icons added to mainmenu component

---
+++ client/src/components/SVG/FilesSVG.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FilesSVG = () => {
+    return (
+        <svg viewBox="0 0 24 24">
+            <polygon points="22.07,8.438 13.632,8.438 13.632,0 " />
+            <path d="M12.074,9.983V0.008H2.037V24h20.203l0.01-14.041L12.074,9.983z" />
+        </svg>
+    );
+};
+
+export default FilesSVG;
