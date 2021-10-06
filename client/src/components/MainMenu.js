45a76e0d7aa3b2c3c3c6cc7c86a7c75fe9384970
Gary <gvk795@gmail.com>
2021-10-05 23:32:10 -0700

    icons added to mainmenu component

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import styles from "./MainMenu.module.css"
 import PushButton from './UI/PushButton'
-import DetailViewSVG from './SVG/DetailViewSVG'
+import ContactsSVG from './SVG/ContactsSVG'
+import EstimatesSVG from './SVG/EstimatesSVG'
+import InvoicesSVG from './SVG/InvoicesSVG'
+import ProjectsSVG from './SVG/ProjectsSVG'
+import TasksSVG from './SVG/TasksSVG'
+import ProductsSVG from './SVG/ProductsSVG'
+import NotesSVG from './SVG/NotesSVG'
+import FilesSVG from './SVG/FilesSVG'
 
 
 const MainMenu = () => {
     return (
         <div className={`${styles.mainMenu} ${'box-shadow'}`}>
             <div className={styles.mainMenu__buttons}>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
-                <PushButton svgElement={<DetailViewSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<ContactsSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<EstimatesSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<InvoicesSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<ProjectsSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<TasksSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<ProductsSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<NotesSVG />} width={"73px"} height={"61px"}/>
+                <PushButton svgElement={<FilesSVG />} width={"73px"} height={"61px"}/>
             </div>
         </div>        
     )
