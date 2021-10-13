bde2db711aaf653b41a05a0304a610c46ffab0c8
Gary <gvk795@gmail.com>
2021-10-12 23:08:08 -0700

    text added for MainMenu portal row

--- client/src/components/UI/MainMenuPortalRow.js
+++ client/src/components/UI/MainMenuPortalRow.js
@@ -9,6 +9,22 @@ const MainMenuPortalRow = () => {
             <div className={styles.mainMenuPortalRow__deleteButton}>
                 <DeleteSVG />
             </div>
+
+            <div className={styles.mainMenuPortalRow__initials}>
+                LC
+            </div>
+
+            <div className={styles.mainMenuPortalRow__nameTitle}>
+                <span>Chaney, Lani</span>
+                <span>Supervisor (Payroll)</span>
+            </div>
+
+            <div className={styles.mainMenuPortalRow__addressPhone}>
+                <span>8310 Iaculis Ave, Annapolis, MD</span>
+                <span>(365) 494-3176</span>
+            </div>
+
+            <div className={styles.mainMenuPortalRow__emailButton}></div>
         </div>
     )
 }
