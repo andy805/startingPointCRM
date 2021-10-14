7c00c60c6b21802792a9be1bc70f0f74e2a8e36d
Gary <gvk795@gmail.com>
2021-10-13 22:51:29 -0700

    mail icon added to MainMenu Portal row

--- client/src/components/UI/MainMenuPortalRow.js
+++ client/src/components/UI/MainMenuPortalRow.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 import styles from "./MainMenuPortalRow.module.css";
-import DeleteSVG from './../SVG/DeleteSVG'
+import DeleteSVG from "./../SVG/DeleteSVG";
+import MailSVG from "../SVG/MailSVG";
 
 const MainMenuPortalRow = () => {
     return (
         <div className={styles.mainMenuPortalRow}>
-
             <div className={styles.mainMenuPortalRow__deleteButton}>
                 <DeleteSVG />
             </div>
 
-            <div className={styles.mainMenuPortalRow__initials}>
-                LC
-            </div>
+            <div className={styles.mainMenuPortalRow__initials}>LC</div>
 
             <div className={styles.mainMenuPortalRow__nameTitle}>
                 <span>Chaney, Lani</span>
@@ -24,9 +22,11 @@ const MainMenuPortalRow = () => {
                 <span>(365) 494-3176</span>
             </div>
 
-            <div className={styles.mainMenuPortalRow__emailButton}></div>
+            <div className={styles.mainMenuPortalRow__emailButton}>
+                <MailSVG />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default MainMenuPortalRow
+export default MainMenuPortalRow;
