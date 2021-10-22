179decc657a9d036a9309c341d50a9af547ae91f
Garrett <gvk795@gmail.com>
2021-10-21 23:17:35 -0700

    sample data added to create MainMenuPortalRows

--- client/src/components/UI/MainMenuPortal.js
+++ client/src/components/UI/MainMenuPortal.js
@@ -3,10 +3,36 @@ import styles from "./MainMenuPortal.module.css";
 import MainMenuPortalRow from './MainMenuPortalRow';
 
 const MainMenuPortal = () => {
+
+    const sampleData = [
+        {
+            firstName: "Lani",
+            lastName: "Chaney",
+            position: "Supervisor (Payroll)",
+            address: "8310 Iaculis Ave, Annapolis, MD",
+            phone: "(365) 494-3176"
+        },
+        {
+            firstName: "Chuck",
+            lastName: "Norris",
+            position: "CEO",
+            address: "320 Dank Ave, Provo Utah",
+            phone: "(458) 777-0123"
+        }
+    ]
+
     return (
         <div className={styles.mainMenuPortal}>
-            <MainMenuPortalRow/>
-        </div>       
+            {sampleData.map(element =>
+                <MainMenuPortalRow
+                    firstName={element.firstName}
+                    lastName={element.lastName}
+                    position={element.position}
+                    address={element.address}
+                    phone={element.phone}
+                />
+            )}
+        </div>
     )
 }
 
